4a53dfefa52458e07fc32c0756b6a8331bf7ceae
kyuyeop <44936443+kyuyeop@users.noreply.github.com>
2022-06-19 18:33:37 +0900

    Update HailMaryProject.java

--- src/hmp/HailMaryProject.java
+++ src/hmp/HailMaryProject.java
@@ -6,12 +6,13 @@ import mindustry.mod.*;
 import static mindustry.Vars.*;
 
 public class HailMaryProject extends Mod{
+    public static NewBlocks newblocks = new NewBlocks();
     @Override
     public void init(){
     }
 
     @Override
     public void loadContent(){
-        NewBlocks.load();
+        newblocks.load();
     }
 }
