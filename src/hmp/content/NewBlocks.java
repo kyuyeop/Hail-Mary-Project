6df1a7438c345c3090a87da5dd5990c1c5c395f4
kyuyeop <44936443+kyuyeop@users.noreply.github.com>
2022-06-19 10:57:32 +0900

    Add files via upload

--- src/hmp/content/NewBlocks.java
+++ src/hmp/content/NewBlocks.java
@@ -23,8 +23,6 @@ import mindustry.world.blocks.heat.*;
 import mindustry.world.blocks.legacy.*;
 import mindustry.world.blocks.liquid.*;
 import mindustry.world.blocks.logic.*;
-import mindustry.world.blocks.payloads.PayloadConveyor;
-import mindustry.world.blocks.payloads.PayloadRouter;
 import mindustry.world.blocks.payloads.*;
 import mindustry.world.blocks.power.*;
 import mindustry.world.blocks.production.*;
@@ -46,21 +44,11 @@ public class NewBlocks implements ContentList{
     public void load(){
         xenonCollector = new GenericCrafter("xenon-collector"){{
             requirements(Category.production, with(Items.copper,750,Items.lead,500,Items.silicon,500,Items.phaseFabric,100,Items.surgeAlloy,50));
-        }};
-        siliconSmelter = new GenericCrafter("silicon-smelter"){{
-            requirements(Category.crafting, with(Items.copper, 30, Items.lead, 25));
-            craftEffect = Fx.smeltsmoke;
-            outputItem = new ItemStack(Items.silicon, 1);
-            craftTime = 40f;
-            size = 2;
+            size = 3;
+            craftTime = 60f;
             hasPower = true;
-            hasLiquids = false;
-            drawer = new DrawMulti(new DrawDefault(), new DrawFlame(Color.valueOf("ffef99")));
-            ambientSound = Sounds.smelter;
-            ambientSoundVolume = 0.07f;
-
-            consumeItems(with(Items.coal, 1, Items.sand, 2));
-            consumePower(0.50f);
+            hasLiquids = true;
+            consumePower(2f);
         }};
     }
 }
